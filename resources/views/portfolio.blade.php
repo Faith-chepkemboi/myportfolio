709c7b147ed49e6510ccd73858e2d13ff84bf689
Tabkil2019 <chepkemoitoo3@gmail.com>
2022-11-28 10:51:49 +0300

    My portfolio1

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <body> 
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Faith Chepkemboi</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="resources/portfolio.css">
+        <link rel="stylesheet" href="assets/css/portfolio.css">
+    </head>
+
+    </body>
+    <h1 class="hey">Faith</h1>
+</html>
+ 
