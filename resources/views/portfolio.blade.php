11104b21a7b03f011b3a6c657123de7dfbd00952
Tabkil2019 <chepkemoitoo3@gmail.com>
2022-12-05 16:12:52 +0300

    commit

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="">
     <body> 
     <head>
         <meta charset="utf-8">
@@ -13,9 +13,133 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="resources/portfolio.css">
         <link rel="stylesheet" href="assets/css/portfolio.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     </head>
-
+ 
     </body>
-    <h1 class="hey">Faith</h1>
+    <!-- herosection -->
+    <div class="hero">
+    
+        <nav>
+            <h2 class="logoo">Faith<span>  Chepkemboi</span></h2>
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Skills</a></li>
+            <li><a href="#">Contact Me</a></li>
+            </ul>
+            <a href="#" class="btn">Call Me</a>
+        </nav>
+        <div class="content">
+        
+            <h4>My name is</h4>
+            <h1>Faith <span>Chepkemboi </span></h1>
+            <h3>I' am a software developer?(mobile & website)</h3>
+            <div class="newslatter">
+                <form action="">
+                    <input type="email" name="email" id="mail" placeholder="Enter Your Email ">
+                    <input type="submit" name="submit" value="Lets Start">
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- about us section -->
+    <section class="about">
+        <div class="main">
+            <img src="assets/images/portfolio3.jpg">
+            <div class="about-text">
+                <h2 style="color: white;">About Me</h2>
+                <h5 style="font-size: 20px; color:white">Mobile & <span> website Developer</span></h5>
+                <p style="color: white;">Iam a young enthusiatic passionate woman in Technology and innovation, who identidys a problem, refines it to a solution and develop a product out of it through coding. </p>
+                <button type="button" class="button">Let's Talk</button>
+
+            </div>
+ 
+        </div>
+
+        <!-- service section -->
+        <div class="service">
+            <div class="title">
+                <h2>My services</h2>
+
+            </div>
+            <div class="box">
+                <div class="card">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+                <h5>Web Development</h5>
+
+                <div class="pra">
+                    <p>i develop websites that meets users expectation and a website that involve the user throughout the process: i use htnkl,css,bootsrap,jquery and javascript </p>
+
+                    <p style="text-align: center;"><a class ="buton" href="#">Read more</a></p>
+
+                </div>
+                </div>
+
+                <div class="card">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+                <h5>Web Development</h5>
+
+                <div class="pra">
+                    <p>i develop websites that meets users expectation and a website that involve the user throughout the process: i use htnkl,css,bootsrap,jquery and javascript </p>
+
+                    <p style="text-align: center;"><a class ="buton" href="#">Read more</a></p>
+
+                </div>
+                </div>
+
+                <div class="card">
+                
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <h5>Web Development</h5>
+
+                <div class="pra">
+                    <p>i develop websites that meets users expectation and a website that involve the user throughout the process: i use htnkl,css,bootsrap,jquery and javascript </p>
+
+                    <p style="text-align: center;"><a class ="buton" href="#">Read more</a></p>
+
+                </div>
+                </div>
+
+                <div class="card">
+                <i class="fa fa-bell" aria-hidden="true"></i>
+                <h5>Web Development</h5>
+
+                <div class="pra">
+                    <p>i develop websites that meets users expectation and a website that involve the user throughout the process: i use htnkl,css,bootsrap,jquery and javascript </p>
+
+                    <p style="text-align: center;"><a class ="buton" href="#">Read more</a></p>
+
+                </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <!-- contact me -->
+        <div class="contact-me">
+            <p>Get me to develope a website/mobile application </p>
+            <a class="button-two" href="#">Hire me</a>
+
+        </div>
+
+       
+
+    </section>
+    <footer>
+            <p>Faith Chepkemboi</p>
+            <p>Email: chepkemoitoo3@gmail.com</p>
+            <div class="social">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-phone"></i></a>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+
+            </div>
+            <p class="end">CopyRights by Faith Chepkemboi</p>
+        </footer>
 </html>
  
